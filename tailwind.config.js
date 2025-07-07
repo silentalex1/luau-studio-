128ecb55e25f7672985af06b226dcbd81abd0e5d
Zal <101911964+silentalex1@users.noreply.github.com>
2025-07-07 15:12:05 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,41 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        gray: {
+          950: '#0a0a0a',
+          900: '#111827',
+          800: '#1f2937',
+          700: '#374151',
+          600: '#4b5563',
+          100: '#f3f4f6',
+        },
+        blue: {
+          300: '#93c5fd',
+          400: '#60a5fa',
+          500: '#3b82f6',
+          600: '#2563eb',
+          700: '#1d4ed8',
+        },
+        green: {
+          600: '#16a34a',
+          700: '#15803d',
+        },
+        emerald: {
+          600: '#059669',
+          700: '#047857',
+        },
+        purple: {
+          600: '#9333ea',
+          700: '#7e22ce',
+        },
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
